504ac2bb774aa3f9e2627e40ad1b78bba0b38d25
Marco Hinz <mh.codebro@gmail.com>
2015-06-09 14:17:11 +0200

    Add bin/book.erl

---
+++ bin/book.erl
@@ -0,0 +1,24 @@
+#!/usr/bin/env escript
+
+-define(BOOKPATH, '/data/books').
+
+list_files(RegExp) ->
+    filelib:fold_files(?BOOKPATH, "((?i)" ++ RegExp ++ ")", true,
+                       fun(File, Acc) -> [File|Acc] end, []).
+
+process_paths(Paths) ->
+    Sorted = lists:sort(fun({A,_}, {B,_}) -> A < B end,
+               [{filename:basename(P), P} || P <- Paths]),
+    lists:zip(lists:seq(1, length(Sorted)), Sorted).
+
+main(Args) ->
+    Paths = process_paths(
+              case length(Args) of
+                  0 -> list_files("");
+                  _ -> list_files(hd(Args))
+              end),
+    [io:format("\e[32m~3w \e[33m~s\n",
+               [Index, PathTail]) || {Index, {PathTail, _}} <- Paths],
+    {ok, [Choice|_]} = io:fread("\e[32mOpen>\e[0m ", "~d"),
+    {_, {_, Path}} = lists:nth(Choice, Paths),
+    os:cmd("open '" ++ Path ++ "'").
