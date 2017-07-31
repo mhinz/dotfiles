44ee922ad27dd020cecd194577a7e5713cc41ff3
Marco Hinz <mh.codebro@gmail.com>
2017-07-31 17:31:09 +0200

    Elixir: change iex colors

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,11 @@
-IEx.configure colors: [
-  eval_error: [:yellow],
-  eval_result: [:blue, :bright]
+IEx.configure [
+  colors: [
+    eval_info:       [:yellow, :bright],
+    eval_error:      [:red],
+    stack_info:      [:blue],
+    doc_code:        [:blue],
+    doc_inline_code: [:blue],
+    doc_headings:    [:red, :bright],
+  ],
+  width: 100,
 ]
