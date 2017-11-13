5ea8a0030b0ad79e98c08688c8daa3f3393a28c7
Marco Hinz <mh.codebro@gmail.com>
2017-11-14 00:57:03 +0100

    Elixir: add macro expander to IEx config

--- .iex.exs
+++ .iex.exs
@@ -16,3 +16,18 @@ IEx.configure [
     "%prefix(%counter)>",
   ] |> IO.ANSI.format |> IO.chardata_to_string
 ]
+
+defmodule Mex do
+  defp expand_all(n, env) do
+    Macro.prewalk(n, &Macro.expand(&1, env))
+  end
+
+  defmacro mex(do: block) do
+    block
+    |> expand_all(__CALLER__)
+    |> Macro.to_string
+    |> IO.puts
+
+    quote do: :ok
+  end
+end
